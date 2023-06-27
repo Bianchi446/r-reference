d5e67ae677f1b94143ba05cf3ea2fde5e2809061
Bianchi446 <josebianchi221js@gmail.com>
2023-06-27 07:10:26 -0300

    using the all function

--- chap2/all-any.R
+++ chap2/all-any.R
@@ -8,4 +8,50 @@ all(x < 20)
 all(x > 0)
 all(x < 1)
 
-# Example : Finding runs of consecutive Ones 
+# Example 1 : Finding runs of consecutive Ones
+
+
+findruns <- function(x, k) {
+
+    n <- length(x)
+    runs <- NULL
+    for(i in 1:(n-k+1)) {
+	if(all(x[i:(i+k-1)] == 1)) runs <- c(runs, i)    
+    }
+
+    return(runs)
+
+}
+
+y <- c(1, 1, 0, 0 , 1, 1, 1 , 0 , 1, 1, 0)
+
+print(findruns(y,2))
+
+
+
+# Example 2 : Finding runs of consecutive Ones (With preallocated memory)
+
+findruns1 <- function(x, k) {
+
+   n <- length(x)
+   runs <- vector(length=n)
+   count <- 0
+   
+   for (i in 1 : (n-k+1)) {
+   
+       if(all(x[i:(i+k-1)] == 1)) {
+           count <- count + 1
+   	   runs[count] <- i
+       }
+   }
+
+   if (count > 0) {
+       runs <- runs[1:count]
+   }   else runs <- NULL
+
+   return(runs)
+
+}
+
+
+print(findruns1(y, 2))
